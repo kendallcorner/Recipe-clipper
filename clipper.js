5bea5fff35a0e3345bb7b4ae5124bada579c5126
Kendall Werts <kmwerts@gmail.com>
2018-11-02 14:09:46 -0500

    got ingredients list

--- clipper.js
+++ clipper.js
@@ -1,5 +1,4 @@
-document.body.textContent = "";
 
-var header = document.createElement('h1');
-header.textContent = "This page has been eaten";
-document.body.appendChild(header);
+
+var Ingredients = document.getElementsByClassName('o-Ingredients');
+console.log("Ingredients: ", Ingredients)
