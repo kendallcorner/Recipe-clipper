6853422ef4589bae6dfd295dff00b376f1b6d958
Kendall Werts <kmwerts@gmail.com>
2018-11-04 15:26:18 -0600

    started tagging food network

--- background.js
+++ background.js
@@ -1,9 +1,7 @@
-browser.contextMenus.create({
-  id: "eat-page",
-  title: "Eat this page"
-});
-
 browser.browserAction.onClicked.addListener(function(tab) {
+    var url = tab.url
+    console.log("url of current tab is ", url)
+
     browser.tabs.executeScript({
       file: "clipper.js"
     });
--- clipper.js
+++ clipper.js
@@ -1,4 +1,51 @@
 
+ListOfTags = {
+    "www.foodnetwork.com": {
+        baseURL: "www.foodnetwork.com",
+        ingredientsTag: "div",
+        ingredients: "o-Ingredients__m-Body",
+        instructionsTag: "div",
+        instructions: "o-Method__m-Body",
+        timeTag: "ul",
+        time: "o-RecipeInfo__m-Time",
+        title: "o-AssetTitle__a-Headline",
+        titleTag: "h1",
+        picture: "m-MediaBlock__a-Image a-Image",
+        pictureTag: "img",
+        yield: "o-RecipeInfo__m-Yield",
+        yieldTag: "ul"
 
-var Ingredients = document.getElementsByClassName('o-Ingredients');
-console.log("Ingredients: ", Ingredients)
+    }, 
+    "www.foodnetwork.com": {
+        baseURL: "www.foodnetwork.com",
+        ingredientsTag: "p",
+        ingredients: "o-Ingredients__a-Ingredient",
+        instructionsTag: "div",
+        instructions: "o-Method__m-Body",
+        timeTag: "ul",
+        time: "o-RecipeInfo__m-Time",
+        title: "o-AssetTitle__a-Headline",
+        titleTag: "h1",
+        picture: "m-MediaBlock__a-Image a-Image",
+        pictureTag: "img",
+        yield: "o-RecipeInfo__m-Yield",
+        yieldTag: "ul"
+
+    }
+    
+}
+
+url = window.location
+baseURL = window.location.host
+console.log("Base URL is ", baseURL)
+
+if (ListOfTags.hasOwnProperty(baseURL)) {
+    siteTags = ListOfTags[baseURL]
+
+    var Ingredients = document.getElementsByClassName(siteTags.ingredients)
+    console.log("Ingredients: ", Ingredients)
+
+
+} else {
+    console.log("Not a known recipe site")
+}
